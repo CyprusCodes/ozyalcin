3dbda447b2b2d9a8b242d31ee43492186880ac41
şira eroğlu <siraeroglu@hotmail.com>
2026-02-25 16:00:05 +0300

    casa oliva deploy update

--- src/config/activeProject.js
+++ src/config/activeProject.js
@@ -2,7 +2,7 @@ import { casaOliva2 } from "./projects/casa-oliva-2";
 import { zenith } from "./projects/zenith";
 import { solea } from "./projects/solea";
 
-//export const activeProject = casaOliva2;
-export const activeProject = zenith;
+export const activeProject = casaOliva2;
+//export const activeProject = zenith;
 //export const activeProject = solea;
 
