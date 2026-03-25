b3ea4b9abdf45654c1ae41fd69bb8703787bdef0
şira eroğlu <siraeroglu@hotmail.com>
2026-03-25 15:45:03 +0300

    zenith google ads

--- src/main.js
+++ src/main.js
@@ -196,6 +196,22 @@ applyBudgetOptions(activeProject);
 // ==============
 
 applyProjectConfig(activeProject);
+// Google Ads tag (SADECE ZENITH)
+if (activeProject?.key === "zenith") {
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = "https://www.googletagmanager.com/gtag/js?id=AW-18038010310";
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-18038010310');
+  `;
+  document.head.appendChild(script2);
+}
 
 // Footer brand logo
 setSrc("footerBrandlogo", activeProject.header?.brandLogo);
